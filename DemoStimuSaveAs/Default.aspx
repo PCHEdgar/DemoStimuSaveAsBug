d7d1f7bd0f1ca589c8dd5936dc439933c2a35015
PCHEdgar <edgar.holzke@pchintl.com>
2016-06-17 12:24:32 +0800

    initial

---
+++ DemoStimuSaveAs/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DemoStimuSaveAs._Default" %>
+
+<%@ Register Assembly="Stimulsoft.Report.MobileDesign" Namespace="Stimulsoft.Report.MobileDesign" TagPrefix="cc1" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div>
+        <cc1:StiMobileDesigner ID="StiMobileDesigner1" runat="server" ShowSaveButton="true"/>
+      </div>
+
+</asp:Content>
---
+++ DemoStimuSaveAs/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.UI;
+using Stimulsoft.Report;
+using Stimulsoft.Report.MobileDesign;
+
+namespace DemoStimuSaveAs
+{
+  public partial class _Default : Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (!IsPostBack)
+      {
+        //TemplateDTO oTemplate = (TemplateDTO)Session["Template"];
+
+        //if (oTemplate != null)
+        {
+          StiReport oReport = new StiReport();
+
+          //if (!string.IsNullOrEmpty(oTemplate.Content))
+          //{
+          //  oReport.LoadFromString(oTemplate.Content);
+          //}
+
+          StiMobileDesigner1.Report = oReport;
+          //}
+          //else
+          //{
+          //  Response.Redirect("~/Templates");
+          //}
+        }
+      }
+    }
+  }
+}
